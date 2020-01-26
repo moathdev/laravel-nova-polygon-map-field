ad7573c81c967cb348904bb0f852dfcbb1802bab
moathdev <moad101@gmail.com>
2020-01-26 09:55:53 +0300

    init

--- README.md
+++ README.md
@@ -14,15 +14,13 @@ composer require moathdev/laravel-nova-Polygon-map-field
 
 use Moathdev\PolygonMap\Polygon;
 use Moathdev\PolygonMap\Point;
-use Moathdev\PolygonMap\ProgressBar;
 
-Polygon::make('map');
-Point::make('map');
-ProgressBar::make('map');
+Polygon::make('map'),
+// or
+Point::make('map'),
 
 
 ```
 
 ## License
-
 The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
--- resources/js/index.js
+++ resources/js/index.js
@@ -15,8 +15,4 @@ Nova.booting((Vue, router, store) => {
   Vue.component("index-polygon-field", require("./components/PolygonIndexField"));
   Vue.component("detail-polygon-field", require("./components/PolygonDetailField"));
   Vue.component("form-polygon-field", require("./components/PolygonFormField"));
-
-  Vue.component("index-progress-bar-field", require("./components/ProgressBarIndexField"));
-  Vue.component("detail-progress-bar-field", require("./components/ProgressBarDetailField"));
-  Vue.component("form-progress-bar-field", require("./components/ProgressBarFormField"));
 });
